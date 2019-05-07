70041ce205d820ad1e468bc689181aedf2997d49
zayzay <1321860742@qq.com>
2019-05-07 21:14:55 +0800

    初步架构

---
+++ gmall-usermanage/src/main/java/com/zay/gmall/gmallusermanage/GmallUsermanageApplication.java
@@ -0,0 +1,13 @@
+package com.zay.gmall.gmallusermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUsermanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallUsermanageApplication.class, args);
+	}
+
+}
---
+++ gmall-usermanage/src/test/java/com/zay/gmall/gmallusermanage/GmallUsermanageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zay.gmall.gmallusermanage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallUsermanageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
